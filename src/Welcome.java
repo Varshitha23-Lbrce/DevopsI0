3bcb6172c784c06824257d2cd496ca8b2fedcef9
AdmiN <AdmiN@ITWS0051>
2024-08-27 11:55:16 +0530

    commit

--- src/Welcome.java
+++ src/Welcome.java
@@ -4,6 +4,7 @@ public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("welcome to devops lab");
 		System.out.println("hiii");
+		System.out.println("good");
 	}
 
 }
